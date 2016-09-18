d999ac149516e98f0c64d3b627e50f7136f64c3d
akash227 <akash227@yahoo.com>
2016-09-18 17:05:52 +0530

    Adding party-details #02

--- client/app.routes.ts
+++ client/app.routes.ts
@@ -1,6 +1,7 @@
 import { RouterConfig, provideRouter } from '@angular/router';
  
 import { PartiesListComponent } from './imports/parties/parties-list.component';
+import { PartyDetailsComponent } from './imports/parties/party-details.component';
  
 // routes array contains the routes of the pages that are going to be used 
 const routes: RouterConfig = [
