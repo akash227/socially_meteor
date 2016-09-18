b6f031790322e2655343adce0c2115db13b76eda
akash227 <akash227@yahoo.com>
2016-09-18 18:19:36 +0530

    Adding to parties-details #12

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
- 
+
 import template from './app.component.html';
- 
+
 @Component({
   selector: 'app',
   template,
--- client/main.js
+++
@@ -1,8 +0,0 @@
-var angular2_meteor_auto_bootstrap_1 = require('angular2-meteor-auto-bootstrap');
-var forms_1 = require('@angular/forms');
-var app_component_1 = require('./app.component');
-angular2_meteor_auto_bootstrap_1.bootstrap(app_component_1.AppComponent, [
-    forms_1.disableDeprecatedForms(),
-    forms_1.provideForms()
-]);
-//# sourceMappingURL=main.js.map
