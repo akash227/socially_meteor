c516a30b7efc04098c54105592783d33ae434371
akash227 <akash227@yahoo.com>
2016-09-18 16:47:05 +0530

    Importing app routes in main

--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,8 @@
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
- 
+
+import { APP_ROUTER_PROVIDERS } from './app.routes';
+
 import { AppComponent } from './app.component';
  
 bootstrap(AppComponent, [
