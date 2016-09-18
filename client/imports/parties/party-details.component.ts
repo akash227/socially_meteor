cbe5c9207a87321efdf6a598c28dbe596f28be99
akash227 <akash227@yahoo.com>
2016-09-18 19:37:04 +0530

    Adding to parties-details #19

--- client/imports/parties/party-details.component.ts
+++ client/imports/parties/party-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, NgZone, OnInit } from '@angular/core';
 import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
 import { Tracker } from 'meteor/tracker';
-
+import { Party } from '../../../both/interfaces/party.interface';
 import { Parties } from '../../../both/collections/parties.collections';
 
 import template from './party-details.component.html';
@@ -14,7 +14,7 @@ import template from './party-details.component.html';
 
 export class PartyDetailsComponent {
 	partyId: string;
- 	party: any;
+ 	party: Party;
 
   	constructor(private route: ActivatedRoute, private ngZone: NgZone) {}
 
