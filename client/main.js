19acb2b7fe7460bce616975a33730913c88d3fd7
akash227 <akash227@yahoo.com>
2016-09-18 15:26:30 +0530

    Modifying form #04

--- client/main.js
+++ client/main.js
@@ -1,4 +1,8 @@
 var angular2_meteor_auto_bootstrap_1 = require('angular2-meteor-auto-bootstrap');
+var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
-angular2_meteor_auto_bootstrap_1.bootstrap(app_component_1.AppComponent);
+angular2_meteor_auto_bootstrap_1.bootstrap(app_component_1.AppComponent, [
+    forms_1.disableDeprecatedForms(),
+    forms_1.provideForms()
+]);
 //# sourceMappingURL=main.js.map
