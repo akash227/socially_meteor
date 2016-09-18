38b0f7f1c7256fbe842e99fb5ec4ff3be4417c12
akash227 <akash227@yahoo.com>
2016-09-18 16:37:29 +0530

    Modifying app comp #00

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,22 +1,9 @@
 import { Component } from '@angular/core';
-import { Mongo } from 'meteor/mongo';
-import { Parties } from '../both/collections/parties.collections';
-import { PartiesFormComponent } from './imports/parties/parties-form.component';
-//noinspection TypeScriptCheckImport
+ 
 import template from './app.component.html';
-
+ 
 @Component({
-    selector: 'app',
-    template,
-    directives: [PartiesFormComponent]
+  selector: 'app',
+  template
 })
-
-export class AppComponent {
-    parties:Mongo.Cursor<any>;
-    constructor() {
-        this.parties = Parties.find();
-    }
-    removeParty(party) {
-    	Parties.remove(party._id);
-  	}
-}
+export class AppComponent {}
