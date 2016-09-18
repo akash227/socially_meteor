5b54f2134ffd14a40976c978bcc88881198890e5
akash227 <akash227@yahoo.com>
2016-09-18 23:29:03 +0530

    Modifying parties#001

--- both/collections/parties.collections.ts
+++ both/collections/parties.collections.ts
@@ -3,3 +3,8 @@ import {Mongo} from 'meteor/mongo';
 import {Party} from '../interfaces/party.interface';
  
 export const Parties = new Mongo.Collection<Party>('parties');
+
+function loggedIn() {
+  return !!Meteor.user();
+}
+
