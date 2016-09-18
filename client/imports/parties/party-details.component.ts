bbc177ce97f008e3aa7382415dcc2351b1522c66
akash227 <akash227@yahoo.com>
2016-09-18 17:40:41 +0530

    Adding to parties-details #03

--- client/imports/parties/party-details.component.ts
+++ client/imports/parties/party-details.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { Parties } from '../../../both/collections/parties.collections';
+
 import template from './party-details.component.html';
- 
+
 @Component({
   selector: 'party-details',
   template
