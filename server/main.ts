ff1f556fd4ca70b398efa22bbee39a7ca0b4f37e
akash227 <akash227@yahoo.com>
2016-09-19 20:35:06 +0530

    mod #06

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,6 @@
 import { loadParties } from './imports/fixtures/parties';
 import { Meteor } from 'meteor/meteor';
+import './imports/publications/parties';
 
 Meteor.startup(() => {
     // load initial Parties
